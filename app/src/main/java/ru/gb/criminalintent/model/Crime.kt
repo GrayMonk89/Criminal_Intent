d0eadca39d3d1fd13a72b1363c9a18aca8aa2fdd
Gray Monk <graymonk89@yandex.ru>
2022-11-01 01:12:25 +0300

    Начинаем упражнение(стр 231)

--- app/src/main/java/ru/gb/criminalintent/model/Crime.kt
+++ app/src/main/java/ru/gb/criminalintent/model/Crime.kt
@@ -7,5 +7,6 @@ data class Crime(
     val id: UUID = UUID.randomUUID(),
     var title: String = "",
     val date: Date = Date(),
-    var isSolved: Boolean = false
+    var isSolved: Boolean = false,
+    var hard: Int = 0
 )
--- app/src/main/java/ru/gb/criminalintent/utils/KeyStorage.kt
+++ app/src/main/java/ru/gb/criminalintent/utils/KeyStorage.kt
@@ -1,5 +1,8 @@
 package ru.gb.criminalintent.utils
 
+const val DEFAULT_VALUE_ZERO = 0
+const val DEFAULT_VALUE_ONE = 1
+const val DEFAULT_VALUE_TWO = 2
 const val TAG_CRIME_LIST_FRAGMENT = "Crime list fragment"
 
 class KeyStorage {
